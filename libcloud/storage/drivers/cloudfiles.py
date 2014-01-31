a30d23a4ea78512ca4a59377272636bbffe30f9e
John Obelenus <jobelenus@gmail.com>
2014-01-31 15:18:34 -0500

    If you know the name of the container, you shouldn't need to make a call to get a container obj/dict first, and then another for the cdn url.

--- libcloud/storage/drivers/cloudfiles.py
+++ libcloud/storage/drivers/cloudfiles.py
@@ -269,6 +269,9 @@ class CloudFilesStorageDriver(StorageDriver, OpenStackDriverMixin):
         cdn_uri = 'x-cdn-ssl-uri' if self.use_cdn_url_https else 'x-cdn-uri'
             
         container_name = container.name
+        return self.get_container_cdn_url_from_name(container_name)
+
+    def get_container_cdn_url_from_name(self, container_name):
         response = self.connection.request('/%s' % (container_name),
                                            method='HEAD',
                                            cdn_request=True)
